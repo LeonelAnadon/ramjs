c7f9b8d977ba1dc6554e548f63265eee03adc69e
Leonel Anadon <leonelanadon95@gmail.com>
2024-07-22 09:08:05 -0300

    se ajusta configuracion sv

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "bin"),
     filename: "main.js",
-    publicPath: "/ramjs",
+    publicPath: "/ramjs/",
     clean: true,
   },
   module: {
